8c9b242cb833aaaec4d061e490e88d1aa114cb63
Finn.Zhu <Promacanthus@gmail.com>
2026-09-05 09:45:55 +0800

    fix: persist recommended Xcode settings and app category

--- scripts/generate_project.py
+++ scripts/generate_project.py
@@ -46,9 +46,27 @@ children = ', '.join(ident(str(p.relative_to(ROOT))) for p in all_files)
 add('main-group', f'isa = PBXGroup; children = ({children}, {ident("info")}, {ident("icon")}, {", ".join(resource_refs)}, {ident("products")},); sourceTree = "<group>";')
 
 common = '''SDKROOT = macosx; MACOSX_DEPLOYMENT_TARGET = 14.0; SWIFT_VERSION = 6.0;
-SWIFT_STRICT_CONCURRENCY = complete; CLANG_ENABLE_MODULES = YES; CODE_SIGN_STYLE = Manual;
-CODE_SIGN_IDENTITY = "-"; ENABLE_APP_SANDBOX = NO;
+SWIFT_STRICT_CONCURRENCY = complete; CLANG_ENABLE_MODULES = YES; CLANG_ENABLE_OBJC_WEAK = YES;
+CODE_SIGN_STYLE = Manual; CODE_SIGN_IDENTITY = "-"; ENABLE_APP_SANDBOX = NO;
+DEAD_CODE_STRIPPING = YES;
 OTHER_LDFLAGS = "$(inherited) -lsqlite3"; COMBINE_HIDPI_IMAGES = YES;'''
+
+recommended_warnings = '''CLANG_WARN_BLOCK_CAPTURE_AUTORELEASING = YES;
+CLANG_WARN_BOOL_CONVERSION = YES; CLANG_WARN_COMMA = YES;
+CLANG_WARN_CONSTANT_CONVERSION = YES; CLANG_WARN_DEPRECATED_OBJC_IMPLEMENTATIONS = YES;
+CLANG_WARN_DIRECT_OBJC_ISA_USAGE = YES_ERROR; CLANG_WARN_DOCUMENTATION_COMMENTS = YES;
+CLANG_WARN_EMPTY_BODY = YES; CLANG_WARN_ENUM_CONVERSION = YES;
+CLANG_WARN_INFINITE_RECURSION = YES; CLANG_WARN_INT_CONVERSION = YES;
+CLANG_WARN_NON_LITERAL_NULL_CONVERSION = YES; CLANG_WARN_OBJC_IMPLICIT_RETAIN_SELF = YES;
+CLANG_WARN_OBJC_LITERAL_CONVERSION = YES; CLANG_WARN_OBJC_ROOT_CLASS = YES_ERROR;
+CLANG_WARN_QUOTED_INCLUDE_IN_FRAMEWORK_HEADER = YES; CLANG_WARN_RANGE_LOOP_ANALYSIS = YES;
+CLANG_WARN_STRICT_PROTOTYPES = YES; CLANG_WARN_SUSPICIOUS_MOVE = YES;
+CLANG_WARN_UNGUARDED_AVAILABILITY = YES_AGGRESSIVE; CLANG_WARN_UNREACHABLE_CODE = YES;
+CLANG_WARN__DUPLICATE_METHOD_MATCH = YES; ENABLE_STRICT_OBJC_MSGSEND = YES;
+GCC_NO_COMMON_BLOCKS = YES; GCC_WARN_64_TO_32_BIT_CONVERSION = YES;
+GCC_WARN_ABOUT_RETURN_TYPE = YES_ERROR; GCC_WARN_UNDECLARED_SELECTOR = YES;
+GCC_WARN_UNINITIALIZED_AUTOS = YES_AGGRESSIVE; GCC_WARN_UNUSED_FUNCTION = YES;
+GCC_WARN_UNUSED_VARIABLE = YES;'''
 
 for target, files in [('app', core + app), ('test', core + [ROOT / 'Veyra/App/MonitorStore.swift'] + tests)]:
     build_files = []
@@ -63,7 +81,7 @@ for target, files in [('app', core + app), ('test', core + [ROOT / 'Veyra/App/Mo
     for config in ['Debug', 'Release']:
         # Keep the existing bundle IDs so a rename preserves saved user preferences.
         flags = 'SWIFT_OPTIMIZATION_LEVEL = "-Onone"; ENABLE_TESTABILITY = YES; DEBUG_INFORMATION_FORMAT = dwarf; SWIFT_ACTIVE_COMPILATION_CONDITIONS = DEBUG;' if config == 'Debug' else 'SWIFT_OPTIMIZATION_LEVEL = "-O"; DEBUG_INFORMATION_FORMAT = "dwarf-with-dsym";'
-        settings = 'PRODUCT_NAME = "Veyra"; PRODUCT_BUNDLE_IDENTIFIER = local.codexmonitor.app; INFOPLIST_FILE = Veyra/Info.plist; LD_RUNPATH_SEARCH_PATHS = "$(inherited) @executable_path/../Frameworks";' if target == 'app' else 'PRODUCT_NAME = VeyraTests; PRODUCT_BUNDLE_IDENTIFIER = local.codexmonitor.tests; GENERATE_INFOPLIST_FILE = YES; TEST_HOST = ""; BUNDLE_LOADER = "";'
+        settings = 'PRODUCT_NAME = "Veyra"; PRODUCT_BUNDLE_IDENTIFIER = local.codexmonitor.app; INFOPLIST_FILE = Veyra/Info.plist; INFOPLIST_KEY_LSApplicationCategoryType = "public.app-category.developer-tools"; LD_RUNPATH_SEARCH_PATHS = "$(inherited) @executable_path/../Frameworks";' if target == 'app' else 'PRODUCT_NAME = VeyraTests; PRODUCT_BUNDLE_IDENTIFIER = local.codexmonitor.tests; GENERATE_INFOPLIST_FILE = YES; TEST_HOST = ""; BUNDLE_LOADER = "";'
         add(f'{target}-{config}', f'isa = XCBuildConfiguration; buildSettings = {{ {common} {hardened_runtime} {flags} {settings} }}; name = {config};')
     add(f'{target}-configs', f'isa = XCConfigurationList; buildConfigurations = ({ident(f"{target}-Debug")}, {ident(f"{target}-Release")},); defaultConfigurationIsVisible = 0; defaultConfigurationName = Release;')
     name = 'Veyra' if target == 'app' else 'VeyraTests'
@@ -72,9 +90,14 @@ for target, files in [('app', core + app), ('test', core + [ROOT / 'Veyra/App/Mo
     add(f'target-{target}', f'isa = PBXNativeTarget; buildConfigurationList = {ident(f"{target}-configs")}; buildPhases = ({phases},); buildRules = (); dependencies = (); name = {name}; productName = {name}; productReference = {ident(f"product-{target}")}; productType = {quoted(product_type)};')
 
 for config in ['Debug', 'Release']:
-    add(f'project-{config}', f'isa = XCBuildConfiguration; buildSettings = {{ CLANG_ENABLE_MODULES = YES; }}; name = {config};')
+    config_settings = 'ENABLE_TESTABILITY = YES; ONLY_ACTIVE_ARCH = YES;' if config == 'Debug' else 'SWIFT_COMPILATION_MODE = wholemodule;'
+    add(f'project-{config}', f'''isa = XCBuildConfiguration; buildSettings = {{
+CLANG_ENABLE_MODULES = YES; DEAD_CODE_STRIPPING = YES; ENABLE_USER_SCRIPT_SANDBOXING = YES;
+{recommended_warnings}
+{config_settings}
+}}; name = {config};''')
 add('project-configs', f'isa = XCConfigurationList; buildConfigurations = ({ident("project-Debug")}, {ident("project-Release")},); defaultConfigurationIsVisible = 0; defaultConfigurationName = Release;')
-add('project', f'''isa = PBXProject; attributes = {{ LastUpgradeCheck = 2600; }};
+add('project', f'''isa = PBXProject; attributes = {{ BuildIndependentTargetsInParallel = YES; LastUpgradeCheck = 2660; }};
 buildConfigurationList = {ident('project-configs')}; compatibilityVersion = "Xcode 14.0";
 developmentRegion = zh_CN; hasScannedForEncodings = 0; knownRegions = (zh_CN, en, Base,);
 mainGroup = {ident('main-group')}; productRefGroup = {ident('products')}; projectDirPath = "";
@@ -95,7 +118,7 @@ def reference(target):
 
 
 (scheme / 'Veyra.xcscheme').write_text(f'''<?xml version="1.0" encoding="UTF-8"?>
-<Scheme LastUpgradeVersion="2600" version="1.3">
+<Scheme LastUpgradeVersion="2660" version="1.3">
   <BuildAction parallelizeBuildables="YES" buildImplicitDependencies="YES"><BuildActionEntries>
     <BuildActionEntry buildForTesting="YES" buildForRunning="YES" buildForProfiling="YES" buildForArchiving="YES" buildForAnalyzing="YES">{reference('app')}</BuildActionEntry>
     <BuildActionEntry buildForTesting="YES" buildForRunning="NO" buildForProfiling="NO" buildForArchiving="NO" buildForAnalyzing="YES">{reference('test')}</BuildActionEntry>
